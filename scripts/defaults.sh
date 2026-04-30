4be913fdfcb5084a7c20ba77c8ff2afabeefaaee
Madeleine Ostoja <madi@madeleineostoja.com>
2026-04-30 14:11:00 +1200

    fix finder default view

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -22,7 +22,7 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 defaults write com.apple.finder AppleShowAllFiles -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
