edd41443cb33ecda65ad9e596f7e091a2d8d818a
Madi Ostoja <madi@madeleineostoja.com>
2026-06-15 09:40:24 +1200

    Fix pnpm globals

--- home.nix
+++ home.nix
@@ -17,7 +17,6 @@ let
   pnpmGlobals = [
     "sentry"
     "@earendil-works/pi-coding-agent"
-    "@schpet/linear-cli"
   ];
 in
 {
@@ -83,7 +82,7 @@ in
     export PATH="${pkgs.nodejs}/bin:${pkgs.pnpm}/bin:$PNPM_HOME/bin:$PATH"
     for pkg in ${lib.escapeShellArgs pnpmGlobals}; do
       if ! ${pkgs.pnpm}/bin/pnpm ls -g --depth=0 2>/dev/null | grep -q "$pkg"; then
-        run ${pkgs.pnpm}/bin/pnpm add -g "$pkg"
+        run ${pkgs.pnpm}/bin/pnpm add -g --ignore-scripts "$pkg"
       fi
     done
   '';
